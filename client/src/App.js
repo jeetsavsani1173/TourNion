d123bec88c5e95421a859ca3a934cc7ad43aa1b9
jeetsavsani1173 <jeetsavsani1173@gmail.com>
2023-02-17 16:21:07 +0530

    Login Form Added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return <>Hye Guys</>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCardFooter,
+  MDBValidation,
+  MDBBtn,
+  MDBIcon,
+  MDBValidationItem,
+  MDBSpinner,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+const Login = () => {
+  const [formValue, setFormValue] = useState(initialState);
+  const { email, password } = formValue;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const onInputChange = (e) => {
+    let { name, value } = e.target;
+    setFormValue({ ...formValue, [name]: value });
+  };
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "450px",
+        alignContent: "center",
+        marginTop: "120px",
+      }}
+    >
+      <MDBCard alignment="center">
+        <MDBIcon fas icon="user-circle" className="fa-2x" />
+        <h5>Sign In</h5>
+        <MDBCardBody>
+          <MDBValidation onSubmit={handleSubmit} className="row g-3">
+            <MDBValidationItem
+              feedback="Please provide your email."
+              invalid
+              className="col-md-12"
+            >
+              <MDBInput
+                label="Email"
+                type="email"
+                value={email}
+                name="email"
+                onChange={onInputChange}
+                required
+                invalid
+                // validation="Please provide your email."
+              />
+            </MDBValidationItem>
+            <MDBValidationItem
+              feedback="Please provide your Password."
+              invalid
+              className="col-md-12"
+            >
+              <MDBInput
+                label="Password"
+                type="password"
+                value={password}
+                name="password"
+                onChange={onInputChange}
+                required
+                invalid
+                // validation="Please provide your Password."
+              />
+            </MDBValidationItem>
+            <div className="col-12">
+              <MDBBtn style={{ width: "100%" }} className="mt-2">
+                Login
+              </MDBBtn>
+            </div>
+          </MDBValidation>
+        </MDBCardBody>
+        <MDBCardFooter>
+          <Link to="/register">
+            <p>Don't have an account ? Sign Up</p>
+          </Link>
+        </MDBCardFooter>
+      </MDBCard>
+    </div>
+  );
+};
+
+export default Login;
