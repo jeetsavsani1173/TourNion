2f504375ee334edb38711406d8075cd031822c3b
jeetsavsani1173 <jeetsavsani1173@gmail.com>
2023-03-03 21:40:08 +0530

    Tour added into redux-toolkit

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -5,3 +5,5 @@ const API = axios.create({ baseURL: "http://localhost:5000" });
 export const signIn = (formData) => API.post("/users/signin", formData);
 export const signUp = (formData) => API.post("/users/signup", formData);
 export const googleSignIn = (result) => API.post("/users/googleSignIn", result);
+
+export const createTour = (tourData) => API.post("/tour", tourData);
