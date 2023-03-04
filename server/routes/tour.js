99e3bfb8bad7069a1008ad912187cb0592c39575
jeetsavsani1173 <jeetsavsani1173@gmail.com>
2023-03-04 08:54:12 +0530

    Auth middleware added

--- server/routes/tour.js
+++ server/routes/tour.js
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
 const router = express.Router();
+import auth from "../middleware/auth.js";
 
 import { createTour, getTours } from "../controllers/tour.js";
 
-router.post("/", createTour);
+router.post("/", auth, createTour);
 router.get("/", getTours);
 
 export default router;
