eb46c1cca2a5190cfbfdc005b23a3ed3d07ad614
D.Poriya <dharmeshkporiya@gmail.com>
2023-03-05 17:12:37 +0530

    adding few methods in user model for future use

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const userSchema = mongoose.Schema({
   name: {
@@ -22,4 +24,23 @@ const userSchema = mongoose.Schema({
   },
 });
 
-export default mongoose.model("User", userSchema);
+// userSchema.pre('save',async function(){
+//   let salt = await bcrypt.genSalt(15);
+//   if(this.password !== undefined)
+//       this.password = await bcrypt.hash(this.password,salt);
+// })
+
+userSchema.methods.comparePassword = async function(password){
+  return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateJWTToken = async function(){
+  const secretKey = 'Kj3bBeeT8rBJAWMnElFuYdpVyo3PjX@p2zQn6lj29skpDOcaV5cLIU6qRhQ33BPLxNq3YUP&jdHr7MnGQWwFN8yfkAj5rgj6YcddGa';
+  // let secretKey = process.env.JWT_SECRET_KEY;
+  let token = jwt.sign({email:this.email,id: this._id,},secretKey,{ expiresIn: '1h' });
+  return token;
+}
+
+const userModel = mongoose.model('User',userSchema);
+
+export default userModel;
