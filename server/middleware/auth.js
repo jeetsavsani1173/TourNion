561b413522e99e3d85561d75257b268c09a7c1b0
D.Poriya <dharmeshkporiya@gmail.com>
2023-03-06 20:04:29 +0530

    modified auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,9 @@
 import jwt from "jsonwebtoken";
 
 const secret = "test";
+// secret key = 'Kj3bBeeT8rBJAWMnElFuYdpVyo3PjX@p2zQn6lj29skpDOcaV5cLIU6qRhQ33BPLxNq3YUP&jdHr7MnGQWwFN8yfkAj5rgj6YcddGa'
 
+// this middleware is used to check if the user is authenticated or not and if authenticated then we will add user id in to created by field of tour.
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
