c352aa9b3d7c8167f306468987ad64f03c87b51f
jeetsavsani1173 <jeetsavsani1173@gmail.com>
2023-03-08 10:43:06 +0530

    Search Tour API added

--- server/controllers/tour.js
+++ server/controllers/tour.js
@@ -45,7 +45,7 @@ export const getToursByUser = async (req, res) => {
 
   const userTours = await TourModel.find({ creator: id });
   res.status(200).json(userTours);
-}
+};
 
 export const deleteTour = async (req, res) => {
   const { id } = req.params;
@@ -58,19 +58,39 @@ export const deleteTour = async (req, res) => {
   } catch (err) {
     res.status(404).json({ message: "Something went wrong." });
   }
-}
+};
 
 export const updateTour = async (req, res) => {
   const { id } = req.params;
   const { title, description, creator, imageFile, tags } = req.body;
   try {
     if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).json({ message: "Tour doesn't exist with id: " + id });
+      return res
+        .status(404)
+        .json({ message: "Tour doesn't exist with id: " + id });
     }
-    const updatedTour = { title, description, creator, imageFile, tags, _id: id };
+    const updatedTour = {
+      title,
+      description,
+      creator,
+      imageFile,
+      tags,
+      _id: id,
+    };
     await TourModel.findByIdAndUpdate(id, updatedTour, { new: true });
     res.status(200).json(updatedTour);
   } catch (err) {
     res.status(404).json({ message: "Something went wrong." });
   }
-}
+};
+
+export const getToursBySearch = async (req, res) => {
+  const { searchQuery } = req.query;
+  try {
+    const title = new RegExp(searchQuery, "i");
+    const tours = await TourModel.find({ title });
+    res.json(tours);
+  } catch (err) {
+    res.status(404).json({ message: "Something went wrong." });
+  }
+};
--- server/routes/tour.js
+++ server/routes/tour.js
@@ -7,15 +7,19 @@ import {
   deleteTour,
   getTour,
   getTours,
+  getToursBySearch,
   getToursByUser,
   updateTour,
 } from "../controllers/tour.js";
 
+// used middleware for Authenticate a User
 router.post("/", auth, createTour);
-router.get("/", getTours);
-router.get("/:id", getTour);
 router.get("/userTours/:id", auth, getToursByUser);
 router.patch("/:id", auth, updateTour);
 router.delete("/:id", auth, deleteTour);
+
+router.get("/", getTours);
+router.get("/:id", getTour);
+router.get("/search", getToursBySearch);
 
 export default router;
